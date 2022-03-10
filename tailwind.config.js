61f02f95ebacb94d7b8eeec74a4343504ccecd63
sansai-SNCT <syunzo1115@icloud.com>
2022-03-10 20:51:48 +0900

    [fix] tailwindを反映されるように

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/pages/**/*.{tsx,js}', './src/components/**/*.{tsx,js}'],
   darkmode: false,
   theme: {
     extend: {},
