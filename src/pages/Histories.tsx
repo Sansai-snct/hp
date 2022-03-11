95aa288871ae540cd8ca6c31bb33fbc08a7abebf
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 23:40:26 +0900

    [add] 経歴ページの追加

---
+++ src/pages/Histories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+import Title from '../components/TItle/Title';
+
+const Histories: FC = () => {
+  return (
+    <section className="lg:mt-32 mt-16">
+      <Title>Histories</Title>
+    </section>
+  );
+};
+export default Histories;
