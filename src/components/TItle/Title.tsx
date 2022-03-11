c9162e82e2f1cd3ee5a2b11276e42b0660f478d8
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 22:48:18 +0900

    [updete] サイズ感の調整

--- src/components/TItle/Title.tsx
+++ src/components/TItle/Title.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 const Title: FC = ({ children }) => {
   return (
-    <div className="flex lg:px-60 px-16 font-normal">
+    <div className="flex lg:px-60 px-8 font-normal">
       <div className="lg:text-4xl text-2xl flex-grow relative font-title">
         {children}
         <span className="absolute left-0 -bottom-1.5  w-full h-1 bg-gradient-to-r from-green-700 to-white" />
