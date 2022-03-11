a0fc966de37a6b7832d17f48c1fc58779dd0a7f7
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 21:36:51 +0900

    [updete] fontの適用

--- src/components/TItle/Title.tsx
+++ src/components/TItle/Title.tsx
@@ -3,8 +3,14 @@ import { FC } from 'react';
 
 const Title: FC = ({ children }) => {
   return (
-    <div>
-      <div className="text-5xl">{children}</div>
+    <div className="flex font-normal pb-1 mb-3">
+      <span className="text-4xl flex-grow relative font-title">
+        {children}
+        <span className="absolute left-0 bottom-0 w-full h-1" />
+      </span>
+      <span className="flex-grow-0 relative">
+        <span className="absolute left-0 bottom-0 w-full h-1" />
+      </span>
     </div>
   );
 };
