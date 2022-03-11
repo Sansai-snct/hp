cfd7b512dd67cc8c7d8fc6d3b4a5c217e42d20de
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 22:17:46 +0900

    [add] pagesを生やした

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+import Title from '../components/TItle/Title';
+
+const About: FC = () => {
+  return (
+    <div>
+      <Title>About</Title>
+    </div>
+  );
+};
+export default About;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { FC } from 'react';
+import About from './About';
+
+const Index: FC = () => {
+  return <About />;
+};
+export default Index;
