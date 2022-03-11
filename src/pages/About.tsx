b3f9cf0587a701e42f232af23e1f976138aa6408
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 22:49:02 +0900

    [updete] 内容の追加,スタイルを設定

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,9 +5,17 @@ import Title from '../components/TItle/Title';
 
 const About: FC = () => {
   return (
-    <div>
+    <section className="lg:mt-32 mt-16">
       <Title>About</Title>
-    </div>
+      <div className="lg:mt-6 lg:px-60 mt-4 px-8">
+        <p className="lg:mb-2 mb-1">名前: 山際駿太</p>
+        <p className="lg:mb-2 mb-1">所属: 鈴鹿高専電子情報工学科</p>
+        <p className="lg:mb-2 mb-1">年齢: 18歳</p>
+        <p className="lg:mb-2 mb-1">
+          担当: Webフロントエンド,モバイルアプリ開発,雑用
+        </p>
+      </div>
+    </section>
   );
 };
 export default About;
