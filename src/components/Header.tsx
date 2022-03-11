81690ee83fbdbd3a120522eb71fc8b2a4b2c6ef9
sansai-SNCT <syunzo1115@icloud.com>
2022-03-12 00:14:43 +0900

    [add] Headerを生やした

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <section>
+      <div className="">Sansai | Shunta Yamagiwa</div>
+    </section>
+  );
+};
+export default Header;
