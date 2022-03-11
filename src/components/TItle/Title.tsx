c0a368e08e7ff5b1ebd1ca32983d92dd6be9e649
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 22:02:38 +0900

    [updete] styleの調整

--- src/components/TItle/Title.tsx
+++ src/components/TItle/Title.tsx
@@ -3,14 +3,14 @@ import { FC } from 'react';
 
 const Title: FC = ({ children }) => {
   return (
-    <div className="flex font-normal pb-1 mb-3">
-      <span className="text-4xl flex-grow relative font-title">
+    <div className="flex lg:px-60 px-16 font-normal">
+      <div className="lg:text-4xl text-2xl flex-grow relative font-title">
         {children}
+        <span className="absolute left-0 -bottom-1.5  w-full h-1 bg-gradient-to-r from-green-700 to-white" />
+      </div>
+      <div className="flex-grow-0 relative">
         <span className="absolute left-0 bottom-0 w-full h-1" />
-      </span>
-      <span className="flex-grow-0 relative">
-        <span className="absolute left-0 bottom-0 w-full h-1" />
-      </span>
+      </div>
     </div>
   );
 };
