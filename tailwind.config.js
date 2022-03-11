a166dc0b346fccc4b312481f79e45f64419a4126
sansai-SNCT <syunzo1115@icloud.com>
2022-03-11 23:51:37 +0900

    [update] ちょっとした変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/pages/**/*.{tsx,js}', './src/components/**/*.{tsx,js}'],
+  content: ['./src/pages/*.{tsx,js}', './src/components/*.{tsx,js}'],
   darkmode: false,
   theme: {
     extend: {
