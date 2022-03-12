cf39b7aa5c2776a30d7329bf6a94f488e73e72ce
sansai-SNCT <syunzo1115@icloud.com>
2022-03-12 23:37:43 +0900

    [update] 文字の大きさ、位置の調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,11 @@ import { FC } from 'react';
 const Header: FC = () => {
   return (
     <section className="">
-      <div className=" bg-h-img sm:bg-w-img bg-cover bg-center flex relative h-screen w-screen items-center text-4xl min-h-8"></div>
+      <div className="bg-h-img sm:bg-w-img bg-cover bg-center relative h-screen w-screen ">
+        <div className="text-white text-3xl md:text-6xl lg:text-8xl py-48 lg:py-80 text-center font-shippori">
+          Shunta Yamagiwa
+        </div>
+      </div>
     </section>
   );
 };
