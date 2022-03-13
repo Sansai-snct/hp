83fc8d197bc33c43d75cb913fde3327b519092de
sansai-SNCT <syunzo1115@icloud.com>
2022-03-13 12:05:51 +0900

    [update] リンク追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import { FC } from 'react';
+import Link from '../components/Link';
 import { Link as Scroll } from 'react-scroll';
+
+import twitter from '../images/twitter.png';
+import github from '../images/github.png';
+
 const Header: FC = () => {
   return (
     <section className="">
       <div className="bg-h-img sm:bg-w-img bg-cover bg-center relative h-screen w-screen ">
         <div className="text-white text-3xl md:text-6xl lg:text-8xl py-48 lg:py-80 text-center font-shippori">
           Shunta Yamagiwa
+        </div>
+        <div className=" absolute top-1/2 -right-0 z-50 translate-y-2/4 scale-50">
+          <Link href="https://github.com/Sansai-snct" target="_blank">
+            <img src={twitter} alt="Twitter's logo" />
+          </Link>
+          <Link href="https://twitter.com/sansai_dev" target="_blank">
+            <img src={github} alt="Github's logo" />
+          </Link>
+          <Link
+            href="https://www.instagram.com/sansai_l4z/"
+            target="_blank"
+          ></Link>
         </div>
         <div className="text-4xl text-center">
           <Scroll
