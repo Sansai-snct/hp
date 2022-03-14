ebe7d70cce7a0171b1fbd03bba180918b40607ef
sansai-SNCT <syunzo1115@icloud.com>
2022-03-14 16:23:39 +0900

    [add] 制作物を載せるコンポーネント

---
+++ src/pages/Works.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+import Title from '../components/Title';
+
+const Works: FC = () => {
+  return (
+    <section className="lg:mt-32 mt-16">
+      <Title>Works</Title>
+      <div className="mt-6 lg:px-60  px-8 "></div>
+    </section>
+  );
+};
+export default Works;
