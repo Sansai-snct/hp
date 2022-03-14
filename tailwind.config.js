2739c5d6d723f9b466a384b1ab471783fcfe4ddb
sansai-SNCT <syunzo1115@icloud.com>
2022-03-14 15:00:46 +0900

    [update] fontの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {
       fontFamily: {
         shippori: ['Shippori Antique B1'],
+        roboto: ['Roboto'],
       },
       backgroundImage: (theme) => ({
         'w-img': "url('../src/images/bg-w.jpg')",
         'h-img': "url('../src/images/bg-h.jpg')",
+        'twitter-img': "url('../src/images/twitter-99.png')",
       }),
     },
   },
