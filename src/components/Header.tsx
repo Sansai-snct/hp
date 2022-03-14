b7f100a83b250cb2f3aef39a0c03d1caf7e02639
sansai-SNCT <syunzo1115@icloud.com>
2022-03-14 15:01:07 +0900

    [update] フォントをrobotoに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,15 +10,23 @@ const Header: FC = () => {
   return (
     <section className="">
       <div className="bg-h-img sm:bg-w-img bg-cover bg-center relative h-screen w-screen ">
-        <div className="text-white text-3xl md:text-6xl lg:text-8xl py-48 lg:py-80 text-center font-shippori">
+        <div className="text-white text-3xl md:text-6xl lg:text-8xl py-48 lg:py-80 text-center font-roboto">
           Shunta Yamagiwa
         </div>
-        <div className=" absolute top-1/2 -right-0 z-50 translate-y-2/4 scale-50">
+        <div className="absolute top-1/4 -right-0 z-50 lg:scale-50">
           <Link href="https://github.com/Sansai-snct" target="_blank">
-            <img src={twitter} alt="Twitter's logo" />
+            <img
+              src={twitter}
+              alt="Twitter's logo"
+              className="lg:mb-16 scale-50 lg:scale-100"
+            />
           </Link>
           <Link href="https://twitter.com/sansai_dev" target="_blank">
-            <img src={github} alt="Github's logo" />
+            <img
+              src={github}
+              alt="Github's logo"
+              className="lg:mb-16 scale-50 lg:scale-100"
+            />
           </Link>
           <Link
             href="https://www.instagram.com/sansai_l4z/"
