d9739ad5d037af58ff9bef1661e9a63d69135ecd
sansai-SNCT <syunzo1115@icloud.com>
2022-03-14 16:24:19 +0900

    [update] worksを読み込み

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import { FC, lazy, Fragment, Suspense } from 'react';
 import Header from '../components/Header';
+
 const About = lazy(() => import('./About'));
 const Activities = lazy(() => import('./Activities'));
+const Works = lazy(() => import('./Works'));
+
 const Index: FC = () => {
   return (
     <Fragment>
+      <Header />
       <Suspense fallback={<></>}>
-        <Header />
         <About />
         <Activities />
+        <Works />
       </Suspense>
     </Fragment>
   );
