f0cbae3e88e892337163102af839ce935bd23d5e
sansai-SNCT <syunzo1115@icloud.com>
2022-03-15 10:08:49 +0900

    [update] 大まかな処理を書いた

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -2,12 +2,21 @@ import * as React from 'react';
 import { FC } from 'react';
 
 import Title from '../components/Title';
+import WorkDetail from '../components/WorkDetail';
+
+import kigalt from '../images/kigalt.jpeg';
 
 const Works: FC = () => {
   return (
     <section className="lg:mt-32 mt-16">
       <Title>Works</Title>
-      <div className="mt-6 lg:px-60  px-8 "></div>
+      <div className="mt-6 lg:px-60  px-8">
+        <WorkDetail
+          name="KigaLT"
+          src={kigalt}
+          content="パソコン甲子園2021で発表したLTプラットフォーム"
+        />
+      </div>
     </section>
   );
 };
