bdf3c6cda6cbe2e52faaf1458ab40a3361097d47
sansai-SNCT <syunzo1115@icloud.com>
2022-03-16 21:32:52 +0900

    [add] Footerを生やした

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <footer className="mt-4 text-center text-gray-400">
+      <div>Copyright © 2022 Shunta Yamagiwa All Rights Reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
