2d5aaec4470fbeb95ef265bcc611ec84377d8995
sansai-SNCT <syunzo1115@icloud.com>
2022-03-16 21:06:39 +0900

    [add] skillページの追加

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+import Title from '../components/Title';
+
+const About: FC = () => {
+  return (
+    <section className="lg:mt-32 mt-16">
+      <Title>Skills</Title>
+      <div className="mt-6 lg:px-60  px-8 "></div>
+    </section>
+  );
+};
+export default About;
