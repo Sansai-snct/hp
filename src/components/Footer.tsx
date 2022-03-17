bf599a6e75b208a20198a6f4d8785e3dbaef33c0
sansai-SNCT <syunzo1115@icloud.com>
2022-03-17 23:02:49 +0900

    [update] 上とのmarginをとった

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <footer className="mt-4 text-center text-gray-400 text-xs md:text-base">
+    <footer className="mt-10 text-center text-gray-400 text-xs md:text-base">
       <div>Copyright © 2022 Shunta Yamagiwa All Rights Reserved.</div>
     </footer>
   );
