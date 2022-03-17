b5f0329627ffb8df8c792ac0d4f9aad886665f73
sansai-SNCT <syunzo1115@icloud.com>
2022-03-17 22:37:49 +0900

    [update] 言語の追加

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -3,12 +3,52 @@ import { FC } from 'react';
 
 import Title from '../components/Title';
 
-const About: FC = () => {
+import js from '../images/js.png';
+import nextjs from '../images/nextjs.png';
+import react from '../images/react.png';
+import ts from '../images/typescript.png';
+import swift from '../images/swift.png';
+import git from '../images/git.png';
+import cpp from '../images/cpp.png';
+import python from '../images/python.png';
+
+const Skills: FC = () => {
   return (
     <section className="lg:mt-32 mt-16">
       <Title>Skills</Title>
-      <div className="mt-6 lg:px-60  px-8 "></div>
+      <div className="mt-6 lg:px-60  px-8 ">
+        <ul className="flex">
+          <li className="bg-white w-24 h-24 mr-8">
+            <img src={swift} alt="Swift's logo" />
+            Swift
+          </li>
+
+          <li className="bg-white w-24 h-24 mr-8">
+            <img src={python} alt="Python's logo" />
+            Python
+          </li>
+
+          <li className="bg-white w-24 h-24 mr-8">
+            <img src={cpp} alt="Cpp's logo" />
+            C++
+          </li>
+          <li className=" w-24 h-24 mr-8">
+            <img src={ts} alt="ts's logo" />
+            Typescript
+          </li>
+
+          <li className=" w-24 md:h-24 mr-8">
+            <img src={js} alt="js's logo" />
+            JavaScript
+          </li>
+
+          <li className="w-24 md:h-24 mr-8">
+            <img src={git} alt="next's logo" />
+            Git
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
-export default About;
+export default Skills;
