c92e519b372fa22ed562874ea3ab0fbdeefe6bf7
sansai-SNCT <syunzo1115@icloud.com>
2022-03-17 16:36:47 +0900

    [update] フォントサイズの調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <footer className="mt-4 text-center text-gray-400">
+    <footer className="mt-4 text-center text-gray-400 text-xs md:text-base">
       <div>Copyright © 2022 Shunta Yamagiwa All Rights Reserved.</div>
     </footer>
   );
