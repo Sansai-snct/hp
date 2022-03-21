5529a924c065f45da3899736bf3641fe255bee10
sansai-SNCT <syunzo1115@icloud.com>
2022-03-21 22:22:48 +0900

    [update] コピーライトの文字色を変更 #3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <footer className="mt-10 md:mt-20 text-center text-gray-400 text-xs md:text-base">
+    <footer className="mt-10 md:mt-20 text-center text-black text-xs md:text-base">
       <div>Copyright © 2022 Shunta Yamagiwa All Rights Reserved.</div>
     </footer>
   );
