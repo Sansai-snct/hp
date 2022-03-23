658cab9f4187daaa7eda07f4da67edf73b75b2dd
sansai-SNCT <syunzo1115@icloud.com>
2022-03-23 23:22:36 +0900

    [update] スクロールボタンの改良 #3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,25 @@ const Header: FC = () => {
         <div className="text-gray-600 md:text-white text-4xl md:text-6xl lg:text-8xl py-72 md:py-48 lg:py-80 text-center font-roboto">
           Shunta Yamagiwa
         </div>
-        <div className="text-center">
+        <div className="text-center text-4xl text-white font-roboto">
           <Scroll
             to="about"
+            href="/"
             smooth={true}
-            className="rounded inline-flex items-center"
+            className="rounded inline-flex items-center "
           >
             <svg
-              className="w-12 h-12 md:w-16 md:h-16  text-white"
+              className="w-12 h-12 md:w-16 md:h-16  inlineblock text-white"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path>
+            </svg>
+            More...
+            <svg
+              className="w-12 h-12 md:w-16 md:h-16  inlineblock text-white"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
