058e110a92c1733b8ba3a6f3b6b63101dcfdbf44
sansai-SNCT <syunzo1115@icloud.com>
2022-03-25 15:01:49 +0900

    [update] 文字とボタンのサイズと位置のバランスを調整 #3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,29 +6,20 @@ import { Link as Scroll } from 'react-scroll';
 const Header: FC = () => {
   return (
     <section className="">
-      <div className="bg-h-img sm:bg-w-img bg-cover bg-center relative h-screen w-screen ">
-        <div className="text-gray-600 md:text-white text-4xl md:text-6xl lg:text-8xl py-72 md:py-48 lg:py-80 text-center font-roboto">
+      <div className="bg-h-img sm:bg-w-img bg-cover bg-center relative h-screen w-screen py-64 md:py-48 lg:py-72 ">
+        <div className="text-gray-600 md:text-white text-4xl md:text-6xl lg:text-8xl  text-center font-roboto">
           Shunta Yamagiwa
         </div>
-        <div className="text-center text-4xl text-white font-roboto">
+        <div className="text-center text-2xl md:text-4xl text-white my-16 md:my-24 font-roboto">
           <Scroll
             to="about"
             href="/"
             smooth={true}
-            className="rounded inline-flex items-center "
+            className="rounded items-center inline-flex"
           >
-            <svg
-              className="w-12 h-12 md:w-16 md:h-16  inlineblock text-white"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path>
-            </svg>
             More...
             <svg
-              className="w-12 h-12 md:w-16 md:h-16  inlineblock text-white"
+              className="w-10 h-10 md:w-16 md:h-16  inlineblock text-white"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
